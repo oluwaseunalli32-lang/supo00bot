d0f06d7dfb3330fa44f1f7429a0bd3b99cc1cbac
oluwaseunalli32-lang <oluwaseunalli32@gmail.com>
2026-07-01 23:21:54 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
     ADMIN_ID: int
-    DATABASE_URL: str
     OPENAI_API_KEY: SecretStr
     
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
